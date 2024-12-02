01d002cf416efee0d4024c37c1daf6437ac11b14
annabreyer <breyer.anna@gmail.com>
2024-12-02 14:03:31 +0100

    Day 2, Part II

--- src/Manager/DayTwoManager.php
+++ src/Manager/DayTwoManager.php
@@ -10,8 +10,8 @@ class DayTwoManager
 {
     public const INCREASE = 'increase';
     public const DECREASE = 'decrease';
-
     public const EQUAL = 'equal';
+    public const DIFFERENCE = 'difference';
 
     public function __construct(private readonly FileReader $fileReader, private readonly string $kernelProjectDir)
     {
@@ -21,7 +21,7 @@ class DayTwoManager
     {
         $input = $this->fileReader->readNewLineSeperatedFile($this->kernelProjectDir.'/data/days/2/input.txt');
         $result[0] = $this->getNumberOfSafeReports($input);
-        $result[1] = 0;
+        $result[1] = $this->getNumberOfSafeReportsWithProblemDampener($input);
 
         return $result;
     }
@@ -30,7 +30,8 @@ class DayTwoManager
     {
         $number = 0;
         foreach ($input as $line) {
-            if ($this->getReportEvaluation($line)) {
+            $values = explode(' ', $line);
+            if ($this->isReportSafe($values)) {
                 ++$number;
             }
         }
@@ -38,36 +39,58 @@ class DayTwoManager
         return $number;
     }
 
-    public function getReportEvaluation(string $line): bool
+    public function getNumberOfSafeReportsWithProblemDampener(array $input): int
     {
-        $values = explode(' ', $line);
+        $number = 0;
+        foreach ($input as $line) {
+            $values = explode(' ', $line);
+            $modifiedReport = $this->getReportEvaluationWithDampener($values);
+            if ($this->isReportSafeWithDampener($modifiedReport)) {
+                ++$number;
+            }
+        }
+
+        return $number;
+    }
+
+    public function getReportEvaluation(array $line): array
+    {
         $evaluation = [];
 
-        foreach ($values as $key => $value) {
-            if (false === isset($values[$key + 1])) {
+        foreach ($line as $key => $value) {
+            if (false === isset($line[$key + 1])) {
                 break;
             }
 
-            if ($value === $values[$key + 1]) {
+            if ($value === $line[$key + 1]) {
                 $evaluation[$key] = self::EQUAL;
                 continue;
             }
 
-            $difference = abs($value - (int) $values[$key + 1]);
-            if ($value < $values[$key + 1] && $difference <= 3) {
+            $difference = abs((int) $value - (int) $line[$key + 1]);
+            if ($difference > 3) {
+                $evaluation[$key] = self::DIFFERENCE;
+                continue;
+            }
+
+            if ($value < $line[$key + 1]) {
                 $evaluation[$key] = self::INCREASE;
                 continue;
             }
 
-            if ($value > $values[$key + 1] && $difference <= 3) {
+            if ($value > $line[$key + 1]) {
                 $evaluation[$key] = self::DECREASE;
-                continue;
             }
-
-            $evaluation[$key] = false;
         }
 
-        if (in_array(false, $evaluation)) {
+        return $evaluation;
+    }
+
+    public function isReportSafe(array $line): bool
+    {
+        $evaluation = $this->getReportEvaluation($line);
+
+        if (in_array(self::DIFFERENCE, $evaluation)) {
             return false;
         }
 
@@ -80,5 +103,33 @@ class DayTwoManager
         }
 
         return true;
+    }
+
+    public function isReportSafeWithDampener(array $line): bool
+    {
+        if ($this->isReportSafe($line)) {
+            return true;
+        }
+
+        $dampenerEvaluation = $this->getReportEvaluationWithDampener($line);
+
+        return $this->isReportSafe($dampenerEvaluation);
+    }
+
+    public function getReportEvaluationWithDampener(array $line): array
+    {
+        $reportLength = count($line);
+
+        for ($i = 0; $i <= $reportLength; ++$i) {
+            $modifiedReport = $line;
+            unset($modifiedReport[$i]);
+            $modifiedReport = array_values($modifiedReport);
+
+            if ($this->isReportSafe($modifiedReport)) {
+                return $modifiedReport;
+            }
+        }
+
+        return $line;
     }
 }
--- tests/Manager/DayTwoManagerTest.php
+++ tests/Manager/DayTwoManagerTest.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types = 1);
 
 namespace App\Tests\Manager;
 
@@ -9,50 +11,114 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class DayTwoManagerTest extends TestCase
 {
+    private const INPUT = [
+        '7 6 4 2 1',
+        '1 2 7 8 9',
+        '9 7 6 2 1',
+        '1 3 2 4 5',
+        '8 6 4 4 1',
+        '1 3 6 7 9',
+    ];
 
     public function testGetReportEvaluation(): void
     {
         $manager = new DayTwoManager(new FileReader(new Filesystem()), 'kernelProjectDir');
 
-        $safeInput = '7 6 4 2 1';
-        $result = $manager->getReportEvaluation($safeInput);
-        self::assertTrue($result);
+        $safeInput  = explode(' ', self::INPUT[0]);
+        $result     = $manager->getReportEvaluation($safeInput);
+        $evaluation = [
+            DayTwoManager::DECREASE,
+            DayTwoManager::DECREASE,
+            DayTwoManager::DECREASE,
+            DayTwoManager::DECREASE,
+        ];
+        self::assertSame($evaluation, $result);
 
-        $unsafeInput = '1 2 7 8 9';
-        $result = $manager->getReportEvaluation($unsafeInput);
-        self::assertFalse($result);
+        $unsafeInput = explode(' ', self::INPUT[1]);
+        $result      = $manager->getReportEvaluation($unsafeInput);
+        $evaluation  = [
+            DayTwoManager::INCREASE,
+            DayTwoManager::DIFFERENCE,
+            DayTwoManager::INCREASE,
+            DayTwoManager::INCREASE,
+        ];
+        self::assertSame($evaluation, $result);
 
-        $unsafeInput = '9 7 6 2 1';
-        $result = $manager->getReportEvaluation($unsafeInput);
-        self::assertFalse($result);
+        $unsafeInput = explode(' ', self::INPUT[2]);
+        $result      = $manager->getReportEvaluation($unsafeInput);
+        $evaluation  = [
+            DayTwoManager::DECREASE,
+            DayTwoManager::DECREASE,
+            DayTwoManager::DIFFERENCE,
+            DayTwoManager::DECREASE,
+        ];
+        self::assertSame($evaluation, $result);
 
-        $unsafeInput = '1 3 2 4 5';
-        $result = $manager->getReportEvaluation($unsafeInput);
-        self::assertFalse($result);
+        $unsafeInput = explode(' ', self::INPUT[3]);
+        $result      = $manager->getReportEvaluation($unsafeInput);
+        $evaluation  = [
+            DayTwoManager::INCREASE,
+            DayTwoManager::DECREASE,
+            DayTwoManager::INCREASE,
+            DayTwoManager::INCREASE,
+        ];
+        self::assertSame($evaluation, $result);
 
-        $unsafeInput = '8 6 4 4 1';
-        $result = $manager->getReportEvaluation($unsafeInput);
-        self::assertFalse($result);
+        $unsafeInput = explode(' ', self::INPUT[4]);
+        $result      = $manager->getReportEvaluation($unsafeInput);
+        $evaluation  = [
+            DayTwoManager::DECREASE,
+            DayTwoManager::DECREASE,
+            DayTwoManager::EQUAL,
+            DayTwoManager::DECREASE,
+        ];
+        self::assertSame($evaluation, $result);
 
-        $safeInput = '1 3 6 7 9';
-        $result = $manager->getReportEvaluation($safeInput);
-        self::assertTrue($result);
+        $safeInput  = explode(' ', self::INPUT[5]);
+        $evaluation = [
+            DayTwoManager::INCREASE,
+            DayTwoManager::INCREASE,
+            DayTwoManager::INCREASE,
+            DayTwoManager::INCREASE,
+        ];
+        $result     = $manager->getReportEvaluation($safeInput);
+        self::assertSame($evaluation, $result);
     }
 
+    public function testIsReportSafe(): void
+    {
+        $manager = new DayTwoManager(new FileReader(new Filesystem()), 'kernelProjectDir');
+
+        $input  = explode(' ', self::INPUT[0]);
+        self::assertTrue($manager->isReportSafe($input));
+
+        $input  = explode(' ', self::INPUT[1]);
+        self::assertFalse($manager->isReportSafe($input));
+
+        $input  = explode(' ', self::INPUT[2]);
+        self::assertFalse($manager->isReportSafe($input));
+
+        $input  = explode(' ', self::INPUT[3]);
+        self::assertFalse($manager->isReportSafe($input));
+
+        $input  = explode(' ', self::INPUT[4]);
+        self::assertFalse($manager->isReportSafe($input));
+
+        $input  = explode(' ', self::INPUT[5]);
+        self::assertTrue($manager->isReportSafe($input));
+    }
 
     public function testGetNumberOfSafeReports(): void
     {
         $manager = new DayTwoManager(new FileReader(new Filesystem()), 'kernelProjectDir');
-        $input   = [
-            '7 6 4 2 1',
-            '1 2 7 8 9',
-            '9 7 6 2 1',
-            '1 3 2 4 5',
-            '8 6 4 4 1',
-            '1 3 6 7 9',
-        ];
-        $result = $manager->getNumberOfSafeReports($input);
+        $result  = $manager->getNumberOfSafeReports(self::INPUT);
         self::assertEquals(2, $result);
     }
 
+    public function testGetNumberOfSafeReportsWithProblemDampener(): void
+    {
+        $manager = new DayTwoManager(new FileReader(new Filesystem()), 'kernelProjectDir');
+        $result  = $manager->getNumberOfSafeReportsWithProblemDampener(self::INPUT);
+        self::assertEquals(4, $result);
+    }
 }
